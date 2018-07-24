50a62505a7775e659306efad1caf99170e9854d7
Lingling Tong <ltong@microsoft.com>
2018-07-24 10:20:08 -0700

    Add unit tests

---
+++ api/ArnimalAPI/AzureMLTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AzureMLTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AzureMLTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f5c6f5cb-ed10-421c-b27a-9ea9627c7f13")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- api/ArnimalAPI/AzureMLTestConsole/Program.cs
+++ api/ArnimalAPI/AzureMLTestConsole/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AzureMLLibrary.Prediction;
 
 namespace AzureMLTestConsole
 {
@@ -10,6 +11,9 @@ namespace AzureMLTestConsole
     {
         static void Main(string[] args)
         {
+            string url = "http://www.cowtownceramics.com/images/Corgi%203%20in.jpg";
+            string test = CSPrediction.MakePredictionRequestByImageUrl(url).Result;
+            Console.WriteLine(test);
         }
     }
 }
