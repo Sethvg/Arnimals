7b5b7409f869ff0ba98dfb1e87ead54a6baf3317
Max Brister <mabriste@microsoft.com>
2018-07-24 23:48:31 -0700

    Allow cross domain requests

--- api/ArnimalAPI/ArnimalService/Startup.cs
+++ api/ArnimalAPI/ArnimalService/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using ArnimalService.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Cors.Internal;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -11,6 +12,18 @@ namespace ArnimalService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll",
+                    builder =>
+                   {
+                       builder
+                        .AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                .       AllowCredentials();
+                    });
+            });
             services.AddDbContext<AnimalContext>(opt => opt.UseInMemoryDatabase("AnimalList"));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
@@ -18,6 +31,7 @@ namespace ArnimalService
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
+            app.UseCors("AllowAll");
             app.UseMvc();
         }
     }
